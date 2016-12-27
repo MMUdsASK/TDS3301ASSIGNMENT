92d67d14d02f4783009bdc396366e304a0c0b046
MMUdsASK <menubearer@gmail.com>
2016-12-27 14:40:58 +0800

    Update server.R

--- PART1/BIKESHARE/server.R
+++ PART1/BIKESHARE/server.R
@@ -13,18 +13,25 @@ shinyServer(function(input, output) {
     selected <- input$selection
     if (selected == 1) {
       # PLOT CODE
-      plot( x = dataset$instant, y = dataset$weathersit)
+      plot( x = datasetX$instant, y = datasetX$weathersit, ylab = "Weather",
+            xlab = "Time")
     }
     else if (selected == 2) {
       
     }
-    
-    
-    
-    
-    
-    
   })
+  
+  output$TEXTOUT <- renderUI(
+    tagList(
+      p("Weather indicators"),
+      p("1 = Normal weather"),
+      p("2 = Cloudy weather"),
+      p("3 = Rainy or snowy weather")
+    )
+    
+    
+    
+  )
   
   
   
