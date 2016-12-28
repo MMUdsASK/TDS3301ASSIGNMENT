ef15b3eeb5d1b2c5a09af935b7c4ca079676e48d
MMUdsASK <menubearer@gmail.com>
2016-12-29 07:40:05 +0800

    Added customizable scatter plot

--- PART1/BIKESHARE/server.R
+++ PART1/BIKESHARE/server.R
@@ -52,13 +52,79 @@ shinyServer(function(input, output) {
       # bar graph
       ggplot(bikeMean3, aes(hour,cnt)) +
         geom_bar(stat="identity")
+    } else if (selected == 4) {
+      # defaults
+      
+      xData <- input$xdata
+      yData <- input$ydata
+      datasetChoice <- input$choice1
+      
+      # slice dataset according to year and season
+      if (datasetChoice == 1) {
+        datasetSC <- datasetD
+      }
+      else if (datasetChoice == 2) {
+        datasetSC <- datasetD[which(datasetD$yr == 0),]
+      }
+      else if (datasetChoice == 3) {
+        datasetSC <- datasetD[which(datasetD$yr == 0 & datasetD$season == 1),]
+      }
+      else if (datasetChoice == 4) {
+        datasetSC <- datasetD[which(datasetD$yr == 0 & datasetD$season == 2),]
+      }
+      else if (datasetChoice == 5) {
+        datasetSC <- datasetD[which(datasetD$yr == 0 & datasetD$season == 3),]
+      }
+      else if (datasetChoice == 6) {
+        datasetSC <- datasetD[which(datasetD$yr == 0 & datasetD$season == 4),]
+      }
+      else if (datasetChoice == 7) {
+        datasetSC <- datasetD[which(datasetD$yr == 1),]
+      }
+      else if (datasetChoice == 8) {
+        datasetSC <- datasetD[which(datasetD$yr == 1 & datasetD$season == 1),]
+      }
+      else if (datasetChoice == 9) {
+        datasetSC <- datasetD[which(datasetD$yr == 1 & datasetD$season == 2),]
+      }
+      else if (datasetChoice == 10) {
+        datasetSC <- datasetD[which(datasetD$yr == 1 & datasetD$season == 3),]
+      }
+      else if (datasetChoice == 11) {
+        datasetSC <- datasetD[which(datasetD$yr == 1 & datasetD$season == 4),]
+      }
+      textStrings <- c('Normalized temperature', 'Normalized feeling temperature', 'Normalized windspeed',
+                       'Normalized humidity', 'Bike count')
+      # slice data according to attribute
+      if (xData  == 1) {
+        xDataOut <- datasetSC$temp
+      } else if (xData == 2) {
+        xDataOut <- datasetSC$atemp
+      } else if (xData == 3) {
+        xDataOut <- datasetSC$windspeed
+      } else if (xData == 4) {
+        xDataOut <- datasetSC$hum
+      } else if (xData == 5) {
+        xDataOut <- datasetSC$cnt
+      }
+      if (yData  == 1) {
+        yDataOut <- datasetSC$temp
+      } else if (yData == 2) {
+        yDataOut <- datasetSC$atemp
+      } else if (yData == 3) {
+        yDataOut <- datasetSC$windspeed
+      } else if (yData == 4) {
+        yDataOut <- datasetSC$hum
+      } else if (yData == 5) {
+        yDataOut <- datasetSC$cnt
+      }
+      
+      
+      # plot
+      ggplot(datasetSC, aes(x = xDataOut, y = yDataOut)) +
+        geom_point() + labs(x = textStrings[as.integer(xData)], y = textStrings[as.integer(yData)])
     }
   })
-  
-  output$TEXTOUT <- renderUI({}
-  )
-  
-  
   
   
   
