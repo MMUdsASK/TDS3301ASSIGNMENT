c8f51d5df3f9d7c65c8d73f1f2ee891bc1cc0f92
MMUdsASK <menubearer@gmail.com>
2017-01-13 23:14:35 +0800

    update

--- PART2/ARM.R
+++ PART2/ARM.R
@@ -3,18 +3,16 @@
 
 library(sqldf)
 library(pmml)
-#Association Rule Mining 
-library("arules");
+library("arules")
 
-rules.all <- apriori(binaryVector)
-rules.all
-inspect(rules.all)
+timing <- proc.time()
 rulesB <- apriori(binaryVector,parameter=list(minlen=2,supp=0.005,conf=0.8))
+timing <- proc.time() - timing
+print(timing)
 quality(rulesB)<-round(quality(rulesB),digits=3)
 
 #Run sorted rulesB in support
 rulesB.sorted<-sort(rulesB,by="support")
-inspect(rulesB.sorted)
 
 # save the rules to xml for Shiny app use
 write.PMML(rulesB.sorted, file = 'rules.xml')
